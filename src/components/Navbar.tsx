2431b0d2f1652515583c2a8bb549cc0e012f63fb
NguyenTuong1998 <you@example.com>
2024-06-13 15:45:01 +0700

    UI profile logined

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,20 @@
 'use client'
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import Link from 'next/link';
 import Image from 'next/image';
 import logo from '../../public/imgs/logo.png'
+import { UserContext } from '@/app/layout';
+import userNavigation from './UI/UserNavigation';
 export default function Navbar() {
-    const [searchBoxVisibility, setSearchBoxVisibility] = useState(false)
+    const [searchBoxVisibility, setSearchBoxVisibility] = useState(false);
+    const { userAuth } = useContext(UserContext) as any;
+    
+    const ImageLink = (src: string) => {
+        return (
+            <img src={src}  />
+            )
+        }
+    
     return (
         <>
             <nav className='navbar'>
@@ -12,9 +22,10 @@ export default function Navbar() {
                     <Image src={logo} className='w-full' alt="logo-dashboard" priority height="100"
                         width="100" />
                 </Link>
-                <div className={`absolute bg-white w-full left-0  
-        top-full mt-0.border-b border-grey py-4 px-[5vw] md:border-0 
-        md:block md:relative md:inset-0 md:p-0 md:w-auto ` + (searchBoxVisibility ? "show" : 'hide')}>
+                <div className={`
+                        absolute bg-white w-full left-0  
+                        top-full mt-0.border-b border-grey py-4 px-[5vw] md:border-0 
+                        md:block md:relative md:inset-0 md:p-0 md:w-auto ` + (searchBoxVisibility ? "show" : 'hide')}>
                     <input
                         type="text"
                         placeholder='Search'
@@ -22,7 +33,7 @@ export default function Navbar() {
                     />
                     <i className='fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl  text-dark-grey'></i>
                 </div>
-                <div className='flex item-center gap-3 md:gap-6 ml-auto'>
+                <div className='flex gap-3 md:gap-6 ml-auto'>
                     <button
                         onClick={() => setSearchBoxVisibility(!searchBoxVisibility)}
                         className='md:hidden bg-grey w-12 h-12 rounded-full flex items-center justify-center'>
@@ -32,12 +43,40 @@ export default function Navbar() {
                         <i className='fi fi-rr-file-edit'></i>
                         <p>Write</p>
                     </Link>
-                    <Link className='btn-dark py-2 ' href='/signin'>
-                        <p>Sign In</p>
-                    </Link>
-                    <Link className='btn-light hidden md:block py-2' href='/signup'>
-                        <p>Sign Up</p>
-                    </Link>
+
+                    {
+                        userAuth?.access_token ?
+                            <>
+                                <Link className='py-2 ' href='/dashboard/notification'>
+                                    <button className='w-12 h-12 rounded-full bg-grey relative hover:bg-black/10'>
+                                        <i className='fi fi-rr-bell text-2xl block'></i>
+                                    </button>
+                                </Link>
+                                <div className='relative'>
+                                    <button className='w-12 h-12 mt-1'>
+                                        <img src={userAuth?.profile_img} 
+                                        alt="logo-dashboard"  
+                                        height="100" 
+                                        width="100"
+                                        className='w-full h-full object-cover rounded-full'
+                                        ></img>
+                                    </button>
+
+                                    <userNavigation/>
+                                </div>
+                            </>
+                            :
+
+                            <>
+                                <Link className='btn-dark py-2 ' href='/signin'>
+                                    <p>Sign In</p>
+                                </Link>
+                                <Link className='btn-light hidden md:block py-2' href='/signup'>
+                                    <p>Sign Up</p>
+                                </Link>
+                            </>
+                    }
+
                 </div>
             </nav>
         </>
---
+++ src/components/UI/UserNavigation.tsx
@@ -0,0 +1,8 @@
+
+export default function UserNavigation () {
+  return (
+    <div>
+      user
+    </div>
+  );
+}
