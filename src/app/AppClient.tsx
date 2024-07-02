0bd6044a417d33cbea6160bce9b78237cdf4ef76
NguyenTuong <nhattuong.nguyen@kingfoodmart.net>
2024-07-02 16:04:29 +0700

    feature filter by category

--- src/app/AppClient.tsx
+++ src/app/AppClient.tsx
@@ -4,6 +4,10 @@ import axios from "axios"
 import { activeTabRef } from "@/components/InpageNavigation"
 export const BlogContext = createContext<{}>({})
 
+interface PageState {
+    tag: string;
+  }
+
 export default function AppClient({ children }: { children: React.ReactNode }) {
   
     const [blogs, setBlogs ] = useState(null) as any
@@ -11,6 +15,10 @@ export default function AppClient({ children }: { children: React.ReactNode }) {
     const [trendingblogs, setTrendingBlogs ] = useState(null) as any
 
     const [pageState, setPageState] = useState('home')
+
+    let filterTag: PageState = {
+        tag: pageState
+    };
     const getLatestBlogs = async () => {
         await axios.get(process.env.VITE_SERVER_DOMAIN + 'latest-blogs')
         .then(({data : {blogs}}) => {
@@ -26,6 +34,15 @@ export default function AppClient({ children }: { children: React.ReactNode }) {
         })
         .catch(err => console.error(err))
     }
+
+    const getBlogByCategory = async(tag: PageState) => {
+        let params = {tag: pageState}
+        await axios.post(process.env.VITE_SERVER_DOMAIN + 'search-blogs', {tag: params.tag})
+        .then(({data : {blogs}}) => {
+            setBlogs(blogs)
+        })
+        .catch(err => console.error(err))
+    }
       
     useEffect(() => {
 
@@ -33,6 +50,8 @@ export default function AppClient({ children }: { children: React.ReactNode }) {
 
         if(pageState == 'home'){
             getLatestBlogs()
+        }else{
+            getBlogByCategory()
         }
 
         if(!trendingblogs){
